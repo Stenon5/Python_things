271fccb6c3b758f2d3ae03751c05f29374d62205
Stenon <earioselgueta@gmail.com>
2023-04-27 18:41:12 -0400

    refactorizacion del programa -> poo

--- main.py
+++ main.py
@@ -1,70 +1,98 @@
-import random 
+import random
 
-options= ('piedra', 'papel', 'tijera')
+options = ('piedra', 'papel', 'tijeras')
 
-computer_wins = 0
-user_wins = 0
+user_wins_numb = 0
+computer_wins_numb = 0
+rounds_numb = 0
 
-rounds = 1
+
+
+
+##                                 funciones de código
+
+def contador(round= 0, user_win= 0, computer_win= 0,):
+    global user_wins_numb, computer_wins_numb, rounds_numb
+    
+    
+    if round == 1:
+        rounds_numb += 1
+    elif user_win == 1:
+        user_wins_numb += 1
+    elif computer_win == 1:
+        computer_wins_numb += 1
+
+        
+ 
+
+
+#                                      reglas de juego
+def piedra(computer_option):
+    if computer_option == 'tijeras':
+        print('user win')
+        contador(user_win=1)
+    if computer_option == 'papel':   #else podría reemplazar estas líneas
+        print('computer win')
+        contador(computer_win= 1)
+
+def papel(computer_option):
+    if computer_option == 'piedra':
+       print('user win')
+       contador(user_win= 1)
+    if computer_option == 'tijeras':
+       print('computer win')
+       contador(computer_win= 1)
+
+def tijera(computer_option):
+    if computer_option == 'papel':
+        print('user win')
+        contador(user_win= 1)
+    if computer_option == 'piedra':
+        print('computer win')     
+        contador(computer_win= 1) 
+      
+    
+
+##                                     inicio
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
 
 while True:
+    print('')
+    print('')
 
-  print('*' * 10)
-  print('ROUND', rounds)
-  print('*' * 10)
 
-  print('computer_wins', computer_wins)
-  print('user_wins', user_wins)
-  
-  user_option = input('piedra, papel o tijera => ')
-  user_option = user_option.lower()
-  
-  if not user_option in options:
-    print('esa opcion no es valida')
-    continue
+    print('*' * 10)
+    print(f"ROUND: {rounds_numb}")
+    print(f"your wins {user_wins_numb}")
+    print(f"computer wins {computer_wins_numb}")
+    print('*' * 10)
+
     
-  computer_option = random.choice(options)
-  
-  print('user option =>', user_option)
-  print('computer option =>', computer_option)
-  
-  if user_option == computer_option:
-    print('empate!')
-  elif user_option == 'piedra':
-    if computer_option == 'tijera':
-      print('piedra gana a tijera')
-      print('user gano!')
-      user_wins += 1
-    else:
-      print('papel gana a piedra')  
-      print('computer gano!')
-      computer_wins += 1
-  elif user_option == 'papel':
-    if computer_option == 'tijera':
-      print('tijera gana a papel')
-      print('computer win')
-      computer_wins += 1
-    else:
-      print('tijera gana a papel')
-      print('user win')
-      user_wins += 1
-  elif user_option == 'tijera':
-    if computer_option == 'papel':
-      print('tijera gana a papel')
-      print('user win')
-      user_wins += 1
-    else:
-      print('piedra gana a tijera')
-      print('computer win')
-      computers_wins += 1
 
-  if user_wins == 2 :
-    print('El ganador es la el usuario')
-    break
+    
+    user_option = input('piedra, papel o tijera => ')      #Empaquetación del input 
+    user_option = user_option.lower()                          #Validacion del input
+#                                                              # 
+    if not user_option in options:                             #
+        print('esa opcion no es valida')                       #    
+        continue                                               #
 
-  if computer_wins == 2:
-    print('El ganador es la computadora')
-    break    
-
-  rounds += 1
-
+    computer_option = random.choice(options)                #valor de la computadora
+        
+    contador(round= 1)                                      #llamado a reglas de juego
+    if user_option == computer_option:       
+        print('Empate!, intenta de nuevo')
+    elif user_option == 'piedra':
+        piedra(computer_option)
+    elif user_option == 'papel':
+        papel(computer_option)
+    elif user_option == 'tijera':
+        tijera(computer_option)
